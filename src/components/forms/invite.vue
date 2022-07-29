0470e3878373cb0c036a41a3a79199a9ddb426ac
Lena <golyasovaelena@flexites.org>
2022-07-29 16:59:54 +0500

    invites + мелкие правки

--- src/components/forms/invite.vue
+++ src/components/forms/invite.vue
@@ -1,118 +1,64 @@
 <template>
     <div class="invite__container">
-        <FormKit v-model="groupValues" type="group">
-            <FormKit :type="simpleText" name="name" />
-            <!-- <FormKit :type="simpleText" name="email" /> -->
-        </FormKit>
-        <pre wrap>{{ groupValues }}</pre>
-
         <FormKit 
-            v-model="formValues"
-            name="invite"
-            preserve
-            type="form"
-            data-loading="showLoaderSending"
-            :value="item"
+            v-model="list"
+            type="form" 
             form-class="$reset invite__form form__edit"
             :actions="false"
-            :disabled="showLoaderSending"
-            :loading="showLoaderSending ? true : undefined"
+            :disabled="false"
             @submit="submitHandler"
         >
-            <div
-                v-for="(item, index) in invites"
-                :key="index"
-            >   
-                <div class="invite__item">
-                    {{ index }}. {{ item }}
-                    <!-- <FormKit :type="simpleText" name="name" /> -->
-                    <FormKitSchema 
-                        :schema="schema" 
-                    />
+            <h2>Пригласить партнеров</h2>
+
+            <FormKit type="list">
+                <div
+                    v-for="(item, index) in invites"
+                    :key="index"
+                >   
+                    {{ index }} 
+                    <FormKit type="group">
+                        <FormKit
+                            name="name"
+                            label="ФИО"
+                            placeholder="Петров Петр"
+                            validation="required"
+                        />
+                        <FormKit
+                            name="email"
+                            label="Email"
+                            placeholder="petrov@mail.ru"
+                            validation="required|email"
+                        />
+                    </FormKit>
+                    <svg 
+                        v-if="item" 
+                        class="svg-icon svg-icon__stop"
+                        @click="deleteInviteForm(index)"
+                    >
+                        <use xlink:href="../../assets/img/icons/icons.svg#stop" />
+                    </svg>
                 </div>
-            </div> 
-            <pre wrap>{{ message }}</pre> 
-            <button 
-                class="button button-outline-green"
-                @click="addInvite"
-            >
-                + Добавить приглашение
-            </button>
-            <button 
-                class="button button-outline-green"
-                :loading="showLoaderSending"
-                @click="submitHandler"
-            >
-                Отправить приглашения
-            </button>
-        <!-- <button 
-            type="submit"
-            form="invite"
-            class="button button-outline-green"
-            :loading="showLoaderSending"
-            @submit="submitHandler"
-        >
-            Отправить приглашения
-        </button> -->
+                <div 
+                    class="button button-outline-green"
+                    @click="addInvite"
+                >
+                    + Добавить приглашение
+                </div>
+                <button 
+                    class="button button-outline-green"
+                    @submit="submitHandler"
+                >
+                    Отправить приглашения
+                </button>
+            </FormKit>
         </FormKit>
-        <!-- <FormKit 
-            id="invite"
-            v-model="formValues"
-            name="invite"
-            preserve
-            type="form"
-            data-loading="showLoaderSending"
-            :value="formData"
-            form-class="$reset invite__form form__edit"
-            :actions="false"
-            :disabled="showLoaderSending"
-            :loading="showLoaderSending ? true : undefined"
-            @submit="submitHandler"
-        >
-            <div 
-                v-for="(item, index) in invites"
-                :key="index"
-                class="invite__item"
-            >
-                {{ index }}. {{ item }}
-                <div class="h3">Приглашение</div>
-                <div class="form__block">
-                    <FormKitSchema 
-                        :schema="schema" 
-                    />
-                </div>
-            </div>
-
-
-
-            <button 
-                class="button button-outline-green"
-                @click="addInvitate"
-            >
-                + Добавить приглашение
-            </button>
-            <button 
-                class="button button-outline-green"
-                :loading="showLoaderSending"
-                @submitInviteHandler="submitHandler"
-            >
-                Отправить приглашения
-            </button>
-        </FormKit> -->
     </div>
 </template>
 <script>
-// import { slice } from 'lodash';
-
-// import { da } from '@formkit/i18n';
-
-    // import { ref } from 'vue'
-    export default {
-        
+    export default {  
         name: 'Invite',
         data() {
             return {
-                // groupValues: ref({}),
                 groupValues: {},
                 simpleText: {
                     type: 'input',
@@ -128,8 +74,6 @@
                         },
                     ],
                 },
-                // showLoaderSending: false,
-                // inviteNumbers: 1,
                 formData: undefined,
                 formValues: this.formData,
                 schema: [
@@ -147,6 +91,7 @@
                         outerClass: 'field--required',
                     }
                 ],
+                list:[],
                 invites: [{
                     name: '',
                     email: ''
@@ -157,18 +102,15 @@
             addInvite() {
                 this.invites.push({name: '', email: ''});
             },
-            deleteInvite() {
-                let myIndex = this.invites.indexOf('two');
-                this.invites.splice(myIndex,1)
+            deleteInviteForm(index) {
+                console.log(index);
+                this.invites.splice(index,1)
             },
             submitHandler(data) {
-                // console.log(data, this.invites, this.formValues)
+                console.log(data);
+                console.log(this.invites);
                 this.$emit('submitInviteHandler', data)
-                // this.$emit('submitInviteHandler', {invites: [{
-                //     name: data.name,
-                //     email: data.email
-                // }]
-                // });
+
             },
         }
 
