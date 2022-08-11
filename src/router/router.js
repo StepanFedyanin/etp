1b39856bdd80283ac5509f59f9591ce0da0b5a40
KOVex <kovex@portal2u.ru>
2022-08-11 16:40:54 +0500

    small fix by checks

--- src/router/router.js
+++ src/router/router.js
@@ -424,7 +424,11 @@ router.beforeEach((to, from, next) => {
             next({ name: 'auth' });
         }
     } else {
-        next();
+        if (to.name === 'home' && store.state.user && store.state.user.id) {
+            next({ name: 'cabinet' });
+        } else {
+            next();
+        }
     }
 });
 
