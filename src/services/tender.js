fcdeb7a024397a7416b70ec555d3fcdf315a817e
Lena <golyasovaelena@flexites.org>
2022-08-12 08:13:16 +0500

    фикс сайдбара, поправила название ссылок

--- src/services/tender.js
+++ src/services/tender.js
@@ -274,4 +274,11 @@ export default class extends REST {
             throw new RESTError(error, 'Ошибка при отмене приглашения в тендер');
         })
     }
+    static getListlInvitation(){
+        return this._post(`/tender/invites/`, {}).then((data) => {
+            return data
+        }).catch((error) => {
+            throw new RESTError(error, 'Ошибка при получении приглашений в тендер');
+        })
+    }
 }
--- src/views/CabinetParticipantInvites.vue
+++ src/views/CabinetParticipantInvites.vue
@@ -4,7 +4,26 @@
         <div class="cabinet news">
             <div class="container">
                 <div class="news__content text">
-                    В разработке
+                    Приглашения к участию в тендерах
+
+                    <div class="tenders">
+                        <template
+                            v-if="showLoaderSending"
+                        >
+                            <div class="tenders__loader loader">
+                                <div class="spinner" /> Загрузка данных
+                            </div>
+                        </template>
+                        <template
+                            v-else-if="tenders && tenders.count"
+                        >
+                            <blockTender
+                                v-for="(tender, index) in tenders.results"
+                                :key="`tender-${index}`"
+                                :tender="tender"
+                            />
+                        </template>
+                    </div>
                 </div>
             </div>
         </div>
@@ -13,18 +32,48 @@
 
 <script>
     // import Breadcrumbs from '@/components/app-breadcrumbs.vue';
+    import { tender as api} from "@/services"
+    import blockTender from '@/components/block-tender.vue';
 
     export default {
         components: {
             // Breadcrumbs
+            blockTender
         },
         mounted() {
         },
         beforeDestroy() {
         },
         created() {
+            this.cancelInvitation();
         },
         methods: {
-        }
+            cancelInvitation(invitesId){
+                api.cancelInvitation(this.tenderId, invitesId).then(res => {
+                    console.log(res);
+                    this.invites = res;
+                }).catch(err => {
+                    this.showLoaderSending = false;
+                    this.$store.dispatch('showError', err);
+                    console.error(err);
+                });
+            },
+            getTenders() {
+                let limit = Number(this.limit)
+                let params = {
+                    limit,
+                    offset: this.offset
+                }
+                this.showLoaderSending = true;
+                api.getTenders(params).then(tenders => {
+                    this.tenders = tenders
+                    this.showLoaderSending = false;
+                    console.log(tenders)
+                }).catch(err => {
+                    this.showLoaderSending = false;
+                    console.error(err)
+                })
+            },
+        },
     };
 </script>
